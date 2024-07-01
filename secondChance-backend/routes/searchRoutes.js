c827ec2ce01d36fe904e5a73a131f0051921b025
tsikot39 <corpusjohnbenedict@gmail.com>
2024-07-01 12:48:16 -0400

    latest-update

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
+require('dotenv').config();
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
     try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
         const db = await connectToDatabase();
-
-        const collection = db.collection("gifts");
-
+        const collection = db.collection(process.env.MONGO_COLLECTION);
         // Initialize the query object
         let query = {};
 
@@ -18,7 +16,7 @@ router.get('/', async (req, res, next) => {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
         }
 
-        // Task 3: Add other filters to the query
+        // Add other filters to the query
         if (req.query.category) {
             query.category = req.query.category;
         }
@@ -29,10 +27,8 @@ router.get('/', async (req, res, next) => {
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        const items = await collection.find(query).toArray();
-
-        res.json(items);
+        const gifts = await collection.find(query).toArray();
+        res.json(gifts);
     } catch (e) {
         next(e);
     }
